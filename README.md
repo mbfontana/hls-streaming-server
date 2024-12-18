e3528947d3d97988e9331e6424c4ff5924fc8221
Matheus Fontana <mbfontana@outlook.com>
2024-12-18 15:28:45 -0300

    adding cors

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Repo created to learn more about Adaptive Video Streaming with HLS (HTTP Live Streaming)
+
+Convert MP4 video file to HLS with ffmpeg
+```
+ffmpeg -i input_video.mp4 \
+  -codec: copy \
+  -hls_time 10 \
+  -hls_list_size 0 \
+  -f hls \
+  filename.m3u8
+```
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
 import express, { Request, Response } from "express";
 import path from "path";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static("uploads"));
-
+app.use(cors());
 // Route to serve the HLS playlist
 app.get("/video/:filename", (req: Request, res: Response) => {
   const { filename } = req.params;
-  const filePath = path.resolve(__dirname, "..", "uploads", filename);
+  const filePath = path.resolve(__dirname, "..", "uploads", "hls", filename);
 
   res.sendFile(filePath, (err) => {
     if (err) {
